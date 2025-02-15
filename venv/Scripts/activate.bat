ac0ca22f8d12b36d62dc76316c98487fd91f4eac
v0l0 <podolsky35alex@gmail.com>
2025-02-15 21:15:28 +0300

    upgrade

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\podolskyfastapi\venv
+set VIRTUAL_ENV=C:\LabaN1a-fastapi-py\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
