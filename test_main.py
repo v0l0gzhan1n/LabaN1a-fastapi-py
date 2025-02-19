336bd1ddfb6c5807e3b27fa305eeaf8641cd4ef4
v0l0 <podolsky35alex@gmail.com>
2025-02-19 11:29:12 +0300

    commit dublicate tests confirm

--- test_main.py
+++ test_main.py
@@ -6,30 +6,6 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-
-
-def test_get_users():
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk2NTU2MjN9.lzobmfk6B7O_QVU6PAKlF9i877zcstjC5H3NXgrVlHA"} 
-    response = client.get("/users/", headers=headers)
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) > 0
-    assert data[0]["username"] == "string7"
-
-def test_create_user():
-    response = client.post(
-    "/register/",
-    json={"username": "testuser4", "email": "testuser4@example.com",
-    "full_name": "Test User", "password": "password123"},
-    )
-    assert response.status_code == 200
-    data = response.json()
-    assert data["username"] == "testuser4"
-    assert data["email"] == "testuser4@example.com"
-
 def test_create_user():
     response = client.post(
         "/register/",
@@ -70,7 +46,7 @@ def test_login_invalid_password():
 
 # Тест получения списка пользователей
 def test_get_users():
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk2NTU2MjN9.lzobmfk6B7O_QVU6PAKlF9i877zcstjC5H3NXgrVlHA"}
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk5NTQ2NzZ9.tMKZJzUX_heuhdbVHxWW4-OdiTCXa9AvKbSXkAwmrmw"}
     response = client.get("/users/", headers=headers)
     assert response.status_code == 200
     data = response.json()
@@ -78,7 +54,7 @@ def test_get_users():
 
 # Тест получения информации о текущем пользователе
 def test_get_current_user():
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk2NTU2MjN9.lzobmfk6B7O_QVU6PAKlF9i877zcstjC5H3NXgrVlHA"}
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk5NTQ2NzZ9.tMKZJzUX_heuhdbVHxWW4-OdiTCXa9AvKbSXkAwmrmw"}
     response = client.get("/users/me", headers=headers)
     assert response.status_code == 200
     data = response.json()
@@ -86,15 +62,15 @@ def test_get_current_user():
 
 # Тест удаления пользователя
 def test_delete_user():
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk2NTU2MjN9.lzobmfk6B7O_QVU6PAKlF9i877zcstjC5H3NXgrVlHA"}
-    response = client.delete("/users/396", headers=headers)
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk5NTQ2NzZ9.tMKZJzUX_heuhdbVHxWW4-OdiTCXa9AvKbSXkAwmrmw"}
+    response = client.delete("/users/402", headers=headers)
     assert response.status_code == 200
-    response = client.delete("/users/396", headers=headers)
+    response = client.delete("/users/402", headers=headers)
     assert response.status_code == 404
 
 # Тест обновления пользователя
 def test_update_user():
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk2NTU2MjN9.lzobmfk6B7O_QVU6PAKlF9i877zcstjC5H3NXgrVlHA"}
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE3Mzk5NTQ2NzZ9.tMKZJzUX_heuhdbVHxWW4-OdiTCXa9AvKbSXkAwmrmw"}
     response = client.put(
         "/users/3",
         headers=headers,
